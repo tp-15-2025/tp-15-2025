ad46017f69ddb79c47a2c35358b1b44ad3e6d498
Marek Čederle <96355171+MeheheCedy22@users.noreply.github.com>
2025-11-07 18:06:37 +0100

    fix file paths

--- src/js/index.js
+++ src/js/index.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
         //    name: "1. Šprint - <CODENAME>",
         //    role: "member",
         //    timestamp: "DD.MM.YYYY - DD.MM.YYYY",
-        //    image: "./assets/img/sprints/<image>",
+        //    image: "./src/assets/img/sprints/<image>",
         //    description: "Lorem ipsum",
         //},
         // Add more members as needed...
@@ -81,32 +81,32 @@ const pdfData = {
     "Zápisnice": [
       {
        title: "Zápisnica 17.10.2025",
-       link: "./docs/Zapisnica_2025_10_17.pdf"
+       link: "./src/docs/Zapisnica_2025_10_17.pdf"
       },
       {
        title: "Zápisnica 20.10.2025",
-       link: "./docs/Zapisnica_2025_10_20.pdf"
+       link: "./src/docs/Zapisnica_2025_10_20.pdf"
       },
       {
        title: "Zápisnica 23.10.2025",
-       link: "./docs/Zapisnica_2025_10_23.pdf"
+       link: "./src/docs/Zapisnica_2025_10_23.pdf"
       },
     ],
     "Retrospektívy": [
       //{
       //  title: "1 Retrospektíva(šprint 1)",
-      //  link: "./docs/dummy.pdf"
+      //  link: "./src/docs/dummy.pdf"
       //},
       //{
       //  title: "2 Retrospektíva  (šprint 2)",
-      //  link: "./docs/dummy.pdf"
+      //  link: "./src/docs/dummy.pdf"
       //},
       // Add more entries as needed...
     ],
     "Metodiky": [
       // {
       //   title: "Metodika 1",
-      //   link: "./docs/dummy.pdf"
+      //   link: "./src/docs/dummy.pdf"
       // },
       // Add more entries as needed...
     ],
@@ -130,7 +130,7 @@ function populateModal(category) {
         <a href="${pdf.link}" class="documents__append--content d-flex justify-content-between align-items-center" target="_blank">
           <p>${pdf.title}</p>
           <span class="wrapper doc">
-            <img src="assets/img/pdf.png" width="32" alt="PDF" />
+            <img src="./src/assets/img/pdf.png" width="32" alt="PDF" />
           </span>
         </a>
       `;
